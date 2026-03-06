98184ed44b96671d7c2ce238fd913c12e164b792
kaikgivaldo01 <drivercontrolcontato@outlook.com>
2026-03-06 15:39:52 -0300

    deletei
    deletei

--- supabase/migrations/20260109175828_120eebde-7c36-468e-9936-1792e6624fd5.sql
+++
@@ -1,2 +0,0 @@
--- Enable pgcrypto extension for gen_random_bytes()
-CREATE EXTENSION IF NOT EXISTS pgcrypto;
--- supabase/migrations/20260112162546_1f8d43df-db77-429b-ad01-451c6768a229.sql
+++
@@ -1,5 +0,0 @@
--- Add description column to plans table
-ALTER TABLE public.plans ADD COLUMN IF NOT EXISTS description text;
-
--- Add popular flag column
-ALTER TABLE public.plans ADD COLUMN IF NOT EXISTS popular boolean NOT NULL DEFAULT false;
